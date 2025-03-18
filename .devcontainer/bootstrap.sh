69de7afcb51fc2304c9c9042688b8eaa90c9fb70
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-03-18 20:14:11 +0100

    feat: minio for development

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -12,3 +12,11 @@ source .venv/bin/activate
 
 pip install "apache-airflow[celery]==2.10.5" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.10.5/constraints-3.12.txt"
 pip install -r requirements.txt
+
+# ---------------------------------------------------------------------------- #
+#                                   Minio                                      #
+# ---------------------------------------------------------------------------- #
+
+wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio_20250228095516.0.0_amd64.deb -O minio.deb
+sudo dpkg -i minio.deb
+rm ./minio.deb
