7d89924d5c8324a738663da6cd4452c443377c72
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-04-15 08:31:41 +0000

    Change icons in TabsBar Fixes #85

--- modules/apps/mobile/src/app/pages/AppMainPage.vue
+++ modules/apps/mobile/src/app/pages/AppMainPage.vue
@@ -1,44 +1,53 @@
 <template>
-  <ion-page>
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/app/home">
-          <ion-icon aria-hidden="true" :icon="home" />
-          <ion-label>Home</ion-label>
-        </ion-tab-button>
+  <IonPage>
+    <IonTabs>
+      <IonRouterOutlet />
+      <IonTabBar slot="bottom">
+        <IonTabButton
+          tab="tab1"
+          href="/app/home"
+        >
+          <IconHome />
+          <IonLabel>Home</IonLabel>
+        </IonTabButton>
 
-        <ion-tab-button tab="tab2" href="/app/library">
-          <ion-icon aria-hidden="true" :icon="library" />
-          <ion-label>Library</ion-label>
-        </ion-tab-button>
+        <IonTabButton
+          tab="tab2"
+          href="/app/library"
+        >
+          <IconSearch />
+          <IonLabel>Library</IonLabel>
+        </IonTabButton>
 
-        <ion-tab-button tab="tab3" href="/app/settings">
-          <ion-icon aria-hidden="true" :icon="cog" />
-          <ion-label>Settings</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-  </ion-page>
+        <IonTabButton
+          tab="tab3"
+          href="/app/settings"
+        >
+          <IconSettings />
+          <IonLabel>Settings</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+  </IonPage>
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { home, library, cog } from 'ionicons/icons';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonPage, IonRouterOutlet } from '@ionic/vue'
+import { IconHome, IconSearch, IconSettings } from '@/app'
 </script>
 
 <style scoped>
-/**
-https://codepen.io/Francesco_Maretti/pen/yLPRvXp
-https://codepen.io/silas/pen/rNYqZoz
-*/
-/* ion-tab-bar {
+ion-tab-bar {
   --border: 0;
   --background: linear-gradient(
-    to top,
-    rgba(247, 247, 247, 1) 0%,
-    rgba(247, 247, 247, 0) 100%
+    to bottom,
+    rgba(var(--ion-background-color-rgb, 255, 255, 255), 0) 0%,
+    rgba(var(--ion-background-color-rgb, 255, 255, 255), 1) 100%
   );
-  backdrop-filter: blur(5px);
-} */
+  padding-top: 50px;
+}
+
+ion-tab-button {
+  --ripple-color: rgba(0, 0, 0, 0);
+}
 </style>
