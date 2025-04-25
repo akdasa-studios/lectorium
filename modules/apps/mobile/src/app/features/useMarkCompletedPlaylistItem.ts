ce5e966db28370383df50531a646df3f5a8c324d
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-04-25 16:38:21 +0000

    feat(mobile): update condition to mark playlist item as completed

--- modules/apps/mobile/src/app/features/useMarkCompletedPlaylistItem.ts
+++ modules/apps/mobile/src/app/features/useMarkCompletedPlaylistItem.ts
@@ -15,7 +15,7 @@ export function useMarkCompletedPlaylistItem() {
   /* -------------------------------------------------------------------------- */
 
   watch(player.position, async (pos) => {
-    if (pos === player.duration.value) {
+    if (pos >= player.duration.value) {
       const playListItem = await dal.playlistItems.getOne(player.trackId.value) 
       playListItem.completedAt = Date.now()
       await dal.playlistItems.updateOne(playListItem._id, playListItem)
