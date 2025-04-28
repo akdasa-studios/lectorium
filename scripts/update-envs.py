cbbcd506c204ca78ead4feff3b7e162a1ee73e28
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-04-28 07:41:51 +0000

    feat(mobile): Inject readonly auth token for production build Fixes #117

--- scripts/update-envs.py
+++ scripts/update-envs.py
@@ -1,14 +1,13 @@
 import os
 
 # Step 1: Read the environment variables
-release      = os.getenv('RELEASE', 'lectorium@dev')
-dist         = os.getenv('DIST', 'dev')
-sentry_dsn   = os.getenv('SENTRY_DSN', '')
-api_url      = os.getenv('API_URL', 'http://localhost:8001/')
-database_url = os.getenv('DATABASE_URL', 'http://localhost:5984/')
-bucket_name  = os.getenv('BUCKET_NAME', 'lectorium-dev')
-
-print(release, dist)
+release             = os.getenv('RELEASE', 'lectorium@dev')
+dist                = os.getenv('DIST', 'dev')
+sentry_dsn          = os.getenv('SENTRY_DSN', '')
+api_url             = os.getenv('API_URL', 'http://localhost:8001/')
+database_url        = os.getenv('DATABASE_URL', 'http://localhost:5984/')
+bucket_name         = os.getenv('BUCKET_NAME', 'lectorium-dev')
+readonly_auth_token = os.getenv('READONLY_AUTH_TOKEN', '')
 
 # Step 2: Define the content template
 config = f"""
@@ -18,7 +17,8 @@ export const ENVIRONMENT = {{
   sentryDsn: "{sentry_dsn}",
   apiUrl: "{api_url}",
   databaseUrl: "{database_url}",
-  bucketName: "{bucket_name}"
+  bucketName: "{bucket_name}",
+  readonlyAuthToken: "{readonly_auth_token}"
 }}
 """
 
