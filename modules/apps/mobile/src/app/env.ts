400317c6ed1fb072395e84d1f54802fdb3a79c76
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-04-28 09:47:23 +0200

    feat(mobile): Inject readonly auth token for production build (#181)
    Fixes #117

--- modules/apps/mobile/src/app/env.ts
+++ modules/apps/mobile/src/app/env.ts
@@ -4,5 +4,13 @@ export const ENVIRONMENT = {
   sentryDsn: '',
   apiUrl: 'http://localhost:8001/',
   databaseUrl: 'http://localhost:5984/',
-  bucketName: 'lectorium-dev'
+  bucketName: 'lectorium-dev',
+  readonlyAuthToken: 
+    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyZWFkb25seSIsImlhdCI6MTc0NDk4' +
+    'MjczOCwiZXhwIjozNzQ0OTg2MzM4LCJyb2xlcyI6WyJyZWFkb25seSJdfQ.FUHE5gM6cI7H-RzsfM' +
+    'KkY7njS0fpW2zDdROIsK7BV4eYf1_eTbgivElHovjOWBpw5Z4ogBC72VVPdTXiABePl4GyBtNLo3d' +
+    'ygjWABCtnju8YVUwzEoMgMcAeghcb8vVN8JuRwc5HRvjrEc5-FsGg0W8qkAl5I4NxyPj8lnpPMrvg' +
+    'nbnVMzNfxHMk4LiV9F-sRCyj2ew1sH_lzRQxTddEUqQHWQypZrXHkeZ2s07HQbHQMe_iAlT0BBiD-' +
+    '7BlW3ycZIuxAkQhj2nF-9AnqFxUwjk7TdNOeKDW1xkQSkHaaqLy51PXe-R7ZkBpcxgSKF4Qwa3bNv' +
+    'TKgOCQdQeje3WvZg'
 }
