6f3b24ed4128bf52c777150b211d07ac1541c412
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-04-30 08:31:22 +0000

    debugging

--- modules/apps/mobile/src/app/composables/useDatabase.ts
+++ modules/apps/mobile/src/app/composables/useDatabase.ts
@@ -16,26 +16,26 @@ export function useDatabase() {
       userData: new Database({ 
         name: 'userData.db', 
         adapter: adapter, 
-        indices: [
-          { name: 'type', fields: ['type'] },
-          { name: 'addedAt', fields: ['addedAt'] },
-          { name: 'taskStatus', fields: [ 'taskStatus' ] },
-          { name: 'trackId', fields: ['trackId'] }
-        ]
+        // indices: [
+        //   { name: 'type', fields: ['type'] },
+        //   { name: 'addedAt', fields: ['addedAt'] },
+        //   { name: 'taskStatus', fields: [ 'taskStatus' ] },
+        //   { name: 'trackId', fields: ['trackId'] }
+        // ]
       }),
       tracks: new Database({
         name: 'tracks.db',
         adapter: adapter,
-        indices: [
-          { name: 'type', fields: ['type'] }
-        ]
+        // indices: [
+        //   { name: 'type', fields: ['type'] }
+        // ]
       }),
       dictionary: new Database({
         name: 'dictionary.db',
         adapter: adapter,
-        indices: [
-          { name: 'type', fields: ['type'] }
-        ]
+        // indices: [
+        //   { name: 'type', fields: ['type'] }
+        // ]
       }),
       index: new Database({ name: 'index.db', adapter }),
     },
