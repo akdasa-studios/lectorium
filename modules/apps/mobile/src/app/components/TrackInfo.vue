ad3f6a6d02377e6c4e50f3b65c7a5e31ddd6250d
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-05-08 16:14:54 +0200

    chore(mobile): small fixes and improvements (#209)

---
+++ modules/apps/mobile/src/app/components/TrackInfo.vue
@@ -0,0 +1,54 @@
+<template>
+  <h3 class="title-block">
+    <span
+      v-if="references[0]"
+      class="reference"
+    >
+      {{ references[0] }}
+    </span>
+    <span
+      v-if="references?.length > 1"
+      class="reference extra"
+    >
+      +{{ references.length-1 }}
+    </span>
+    <span class="title">{{ title }}</span>
+  </h3>
+</template>
+
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  references: string[]
+}>()
+</script>
+
+<style scoped>
+.title-block {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+
+.title {
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+.reference {
+  background-color: var(--ion-color-light-shade);
+  font-weight: bold;
+  color: var(--ion-color-medium);
+  border-radius: 5px;
+  padding: 0px 5px;
+  font-size: 0.8em;
+  font-stretch: condensed;
+}
+
+.reference.extra {
+  color: var(--ion-color-medium);
+  font-stretch: condensed;
+  opacity: .5;
+}
+</style>
