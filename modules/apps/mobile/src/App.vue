ca206a8eaf30d2e86cbe2347d59ab136c4db8be3
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-05-09 14:48:08 +0000

    feat: add demo functionality to handle specific host styling and navigation in App.vue

--- modules/apps/mobile/src/App.vue
+++ modules/apps/mobile/src/App.vue
@@ -12,6 +12,7 @@ import { onMounted } from 'vue'
 import { IonApp, IonRouterOutlet } from '@ionic/vue'
 import { NavigationBarHolder, useSyncService, useSafeOperation } from '@lectorium/mobile/app'
 import { Player, Transcript } from '@lectorium/mobile/player'
+import { useRouter } from 'vue-router'
 
 /* -------------------------------------------------------------------------- */
 /*                                Dependencies                                */
@@ -19,6 +20,29 @@ import { Player, Transcript } from '@lectorium/mobile/player'
 
 const syncService = useSyncService()
 const safeOperation = useSafeOperation()
+const router = useRouter()
+
+/* -------------------------------------------------------------------------- */
+/*                                    Demo                                    */
+/* -------------------------------------------------------------------------- */
+
+// Demo 
+try {
+  if (window.location.host === 'mobile.listentosadhu.app') {
+    const css = `
+    body {
+      -webkit-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      touch-action: manipulation;
+    }`
+    const head = document.head || document.getElementsByTagName('head')[0]
+    const style = document.createElement('style')
+    head.appendChild(style)
+    style.appendChild(document.createTextNode(css))
+    router.replace({ name: 'search' })
+  }
+} catch(e) { console.log(e) }
 
 /* -------------------------------------------------------------------------- */
 /*                                    Hooks                                   */
