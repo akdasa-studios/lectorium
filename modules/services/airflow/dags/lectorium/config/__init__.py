895d7bd9e3c43c76b8c64b9277f9762ccc302909
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-05-10 17:23:34 +0000

    fix(airflow): remove vastai

--- modules/services/airflow/dags/lectorium/config/__init__.py
+++ modules/services/airflow/dags/lectorium/config/__init__.py
@@ -1,6 +1,5 @@
 from lectorium.config.database import *
 from lectorium.config.app_bucket import VAR_APP_BUCKET_GENERATE_TEMPORARY_ACCESS_KEY, VAR_APP_BUCKET_ACCESS_KEY, VAR_APP_BUCKET_NAME, AppBucketAccessKey, AppBucketGenerateTempraryAccessKey
-from lectorium.config.vastai import VASTAI_ACCESS_KEY, VASTAI_PRIVATE_SSH_KEY
 from lectorium.config.lectorium import *
 from lectorium.config.pools import *
 from lectorium.config.transcripts_proofread import *
--- modules/services/airflow/dags/lectorium/config/lectorium.py
+++ modules/services/airflow/dags/lectorium/config/lectorium.py
@@ -5,19 +5,12 @@ from airflow.models import Variable
 # ---------------------------------------------------------------------------- #
 
 LECTORIUM_VAKSHUDDKI_MINIMUM_AUDIOFILES = "lectorium::vakshuddhi::minimim-audiofiles"
-LECTORIUM_VAKSHUDDKI_VASTAI_QUERY = "lectorium::vakshuddhi::vastai-query"
 BASE_URL = "lectorium::base-url"
 
 
 # ---------------------------------------------------------------------------- #
 #                                    Default                                   #
 # ---------------------------------------------------------------------------- #
-
-Variable.setdefault(
-  LECTORIUM_VAKSHUDDKI_VASTAI_QUERY,
-  "cuda_vers=12.4 num_gpus=1 gpu_name=RTX_4090 inet_down>=100 rentable=true geolocation in DE,BG,EE,FI,IT,MD,NO",
-  "Vast.ai query for Vakshuddhi instance"
-)
 
 Variable.setdefault(
   BASE_URL,
