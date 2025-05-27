5e2efba4df6229d834fc7fbbef5ce75fc12b7c84
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2025-05-27 11:06:09 +0000

    fix(mobile): track completition mark

--- modules/apps/mobile/src/features/playlist/composables/useMarkCompletedPlaylistItem.ts
+++ modules/apps/mobile/src/features/playlist/composables/useMarkCompletedPlaylistItem.ts
@@ -8,16 +8,21 @@ export function useMarkCompletedPlaylistItem() {
   /*                                Dependencies                                */
   /* -------------------------------------------------------------------------- */
 
+  const dal = useDAL() // TODO: useDAL should be injected as a dependency
   const player = usePlayerControls()
-  const dal = useDAL()
 
   /* -------------------------------------------------------------------------- */
   /*                                    Hooks                                   */
   /* -------------------------------------------------------------------------- */
 
   watch(player.position, async (pos) => {
-    if (pos >= player.duration.value) {
-      const playListItem = await dal.playlistItems.getOne(player.playlistItemId.value) 
+    const isTrackAlmostCompleted = player.duration.value - pos < 10
+
+    if (isTrackAlmostCompleted) {
+      const playListItem = await dal.playlistItems.getOne(player.playlistItemId.value)
+      if (playListItem.completedAt) { 
+        return // If completedAt is already set, do not update it again
+      }
       playListItem.completedAt = Date.now()
       await dal.playlistItems.updateOne(playListItem._id, playListItem)
     }
